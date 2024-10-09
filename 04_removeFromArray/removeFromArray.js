b40c1eed546c1785f41182e60e098e010c58d309
Abdulrofihi Musa <m.abdulrofihi@gmail.com>
2024-10-09 12:26:32 +0100

    Add solution for 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    // let formattedArray = [];
+
+    // // Loop through each element in the original array
+    // for(let i = 0; i < array.length; i++) {
+    //     // If the element is not in 'args', add it to the new array
+    //     if (!args.includes(array[i])) {
+    //         formattedArray.push(array[i]);
+    //     }
+    // }
+
+    // return formattedArray;
+
+    return array.filter((item) => !args.includes(item));
 };
 
 // Do not edit below this line
