98121f38956052373a3bf62857acf2688590ca06
Abdulrofihi Musa <m.abdulrofihi@gmail.com>
2024-10-09 11:04:49 +0100

    Add solution for 01_helloWorld and 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,21 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    /**
+     * create an empty string
+     * run a loop num times
+     * concatenate 'string' to the empty string variable on each loop
+     */
 
+    let repeatedString = '';
+
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        for(let i = 0; i < num; i++) {
+            repeatedString += string;
+        }
+    }
+
+    return repeatedString;
 };
 
 // Do not edit below this line
