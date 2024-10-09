51fd42e7585c36cfd179a86b1637f7e51300995b
Abdulrofihi Musa <m.abdulrofihi@gmail.com>
2024-10-09 11:30:05 +0100

    Add solution for 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    /**
+     * get the length of the string
+     * loop through the string and store its letters in an array
+     * access the array from the last index and concatenate dem letters to a string
+     */
+    let maxIndex = string.length - 1;
+    let reversedString = '';
+    let letters = string.split("");
 
+    for(let i = maxIndex; i >= 0; i--) {
+        reversedString += letters[i];
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line
