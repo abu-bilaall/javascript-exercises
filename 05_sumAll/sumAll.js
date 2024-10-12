add6cad5737ba5bf973ba79c9154f7f9a7c8ad12
Abdulrofihi Musa <m.abdulrofihi@gmail.com>
2024-10-12 10:53:00 +0100

    Add solution for 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let totalSum = 0;
 
+    // handling edge cases
+    if (!Number.isInteger(start) || !Number.isInteger(end)) {
+        return "ERROR";
+    }
+    else if (start < 0 || end < 0) {
+        return "ERROR";
+    } else if (start > end) {
+        let temp = end;
+        end = start;
+        start = temp;
+    }
+
+    // 
+    for (let i = start; i <= end; i++) {
+        totalSum += i;
+    }
+
+    return totalSum;
 };
 
 // Do not edit below this line
