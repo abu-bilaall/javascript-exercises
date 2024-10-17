69f256591261e4fffff86e43e80cf16c45b50ed9
Abdulrofihi Musa <m.abdulrofihi@gmail.com>
2024-10-17 13:57:56 +0100

    Add solution for 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  /**
+   * (x − 32) × ⁠5/9⁠ °C
+   * 
+   */
+  let result = (temp - 32) * (5/9);
+  result = Math.round(result * 10) / 10;
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  /**
+   * (x × ⁠9/5⁠ + 32) °F
+   * 
+   */
+
+  let result = (temp * (9/5)) + 32;
+  result = Math.round(result * 10) / 10;
+  return result;
 };
 
 // Do not edit below this line
