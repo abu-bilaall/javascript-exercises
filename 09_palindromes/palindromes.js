c8574b0f2b57071f06d7e577469247ee53a7b005
Abdulrofihi Musa <m.abdulrofihi@gmail.com>
2024-10-21 12:20:55 +0100

    Add solution to 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
+   /**
+    * convert the string into an array
+    * create a copy of the array and reverse it
+    * compare the reversed array w/ the original array
+    */
+   const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+   const cleanedString = string
+   .toLowerCase()
+   .split('')
+   .filter((character) => alphanumerical.includes(character))
+   .join('');
+
+   const reversedString = cleanedString.split('').reverse().join('');
+
+   return cleanedString === reversedString;
 };
 
 // Do not edit below this line
